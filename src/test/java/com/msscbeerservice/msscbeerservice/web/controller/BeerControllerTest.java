fc1f7ad4188b715b00ee16cc08654520c50701a8
Jerome Cortez <jerome.cortez@opsolutions.biz>
2021-12-14 14:52:39 +0800

    Add beer controller JUnit Test

---
+++ src/test/java/com/msscbeerservice/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,56 @@
+package com.msscbeerservice.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msscbeerservice.msscbeerservice.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoToJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoToJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoToJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/" + UUID.randomUUID())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(beerDtoToJson))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteBeerBydId() {
+    }
+}
